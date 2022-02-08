9785f1642e1a879eb10568d07675e5f9445a5cda
Ankit srivastava <99071276+Sas-n3213@users.noreply.github.com>
2022-02-08 16:32:31 +0530

    Add files via upload

---
+++ PrimeNumberCheck.java
@@ -0,0 +1,34 @@
+
+
+import java.util.Scanner;
+
+public class PrimeNumberCheck {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num= sc.nextInt();
+		System.out.println("Enter the Number:");
+	
+		
+		
+		int temp=num;
+		int rev=0,rem;
+		while(temp!=0)
+		{
+		rem=temp%10;
+		rev=rev*10+rem;
+		temp=temp/10;
+		}
+		if(num==rev)
+		{
+		System.out.println(num+"is palindrome number");
+		}
+		else
+		{
+		System.out.println(num+"is not a palindrome number");
+		}
+		}
+		}
